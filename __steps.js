d76f22ba55815557545c44f08e618207cd427964
ZeroBug <404.zerobug@gmail.com>
2022-04-24 14:43:31 +0600

    server created for geius car

---
+++ __steps.js
@@ -0,0 +1,25 @@
+//Setup a server
+/**
+ * 1.Create a file by 'mkdir' or manually
+ * 2.Run these command - npm init -y to create the package.json file
+ * 3.create a index.js file
+ * 4. run 'npm i express cors mongodb dotevt' to install them for the project.
+ * 5. follow get started steps on express.js site to setup express.
+ * 6. require or import cors
+ * 7. require or import dotenv
+ * 8. set port to { process.env.PORT || 5000 }
+ * 9. add 2 middleware
+ * 10. add {"start" and "start-dev"} scripts in package.json file
+ */
+
+/**
+//setup mongodb database
+------------------------
+ * 1. Create a new database for new project from collections
+ * 2. Insert data as document if needed.
+ * 3. Go for node mongo CRUD operation
+ * 4. Go to Database > Connect > connect your application > copy-paste and modify as needed.
+ * 5. on the 'uri' set the user and pass as a environment variable by "dotenv"
+ * 6. create async run funtion for the connection
+ * 7. add routes as per needs
+ */
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//mongodb connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g7wbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+    try{
+        await client.connect();
+        const servicesCollection = client.db('geniusCar').collection('services');
+        
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.send(service)
+        })
+
+        app.post('/services', async(req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        app.delete('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await servicesCollection.deleteOne(query)
+            res.send(result);
+        })
+    }
+    finally{
+    
+    }
+}
+run().catch(console.dir)
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Genius Car!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
